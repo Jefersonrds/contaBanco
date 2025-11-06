041f47126ab7820dd70bb6e92ec6526d6210e2c2
Jefersonrds <contatojefersonrds@gmail.com>
2025-11-06 12:22:51 -0300

    Criar classe contaBanco
    Criar classe com os atributos e metodos definidos no escopo do projeto

---
+++ contaBanco/src/Conta/contaBanco.java
@@ -0,0 +1,24 @@
+package Conta;
+
+public class contaBanco {
+    private int numConta;
+    private String tipo;
+    private String dono;
+    private double saldo;
+    private boolean status;
+
+    public void abrirConta(){
+
+    }
+    public void fecharConta(){
+
+    }
+    public void depositar(){
+
+    }
+    public void sacar(){
+
+    }
+    public void pagarMensal(){}
+    
+}
